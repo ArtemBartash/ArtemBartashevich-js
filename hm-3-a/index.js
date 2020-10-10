231b60c2e3b30b04dfb9d1072fb366844ed7e2bb
ArtemBartashevich <69053070+ArtemBartash@users.noreply.github.com>
2020-10-10 22:50:37 +0300

    feat: new year code

--- hm-3-a/index.js
+++ hm-3-a/index.js
@@ -1,4 +1,40 @@
-let n = 1997;
-let m = 1997 % 12;
-
-console.log(m)
+let year = 1997;
+switch(year % 12)
+{
+    case 0:
+        console.log("Monkey");
+        break;
+    case 1:
+        console.log("Cock");
+        break;
+    case 2:
+        console.log("Dog");
+        break;   
+    case 3:
+        console.log("Pig");
+        break;
+    case 4:
+        console.log("Rat");
+        break;
+    case 5:
+        console.log("Ox");
+        break;
+    case 6:
+        console.log("Tiger");
+        break;
+    case 7:
+        console.log("Rabbit");
+        break;    
+    case 8:
+        console.log("Dragon");
+        break;
+    case 9:
+        console.log("Snake");
+        break;
+    case 10:
+        console.log("Horse");
+        break;
+    default:
+        console.log("Goat");
+        break;             
+}
