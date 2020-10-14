0793f79e5feccbe6a8a2e5c1eb7443758fe52045
ArtemBartashevich <69053070+ArtemBartash@users.noreply.github.com>
2020-10-14 22:49:54 +0300

    feat: homework

--- hm-3/index.js
+++ hm-3/index.js
@@ -1,3 +1,34 @@
+//Запросите у пользователя число, возведите это число во 2-ю степень и выведите на экран.
+/*let num = +process.argv[2];
+console.log(num**2);*/
+
+//Запросите у пользователя 2 числа и выведите среднее арифметическое этих чисел.
+/*let num1 = +process.argv[2];
+let num2 = +process.argv[3];
+let arif = (num1 + num2) / 2;
+console.log(arif)*/
+
+//Запросите у пользователя длину стороны квадрата и выведите площадь такого квадрата.
+
+/*let side = +process.argv[2];
+let square = side**2;
+console.log(square);*/
+
+//Реализуйте конвертор из километров в мили (пользователь вводит километры, программа выводит мили). 1 км = 0,621371 миль. Это значение укажите в коде как константу.
+/*const km_in_miles = 0.621371;
+let kilom = +process.argv[2];
+let result = kilom * km_in_miles;
+console.log(result)*/
+
+//Калькулятор
+/*let num1 = +process.argv[2]
+let num2 = +process.argv[3]
+let sum = number1 + number2
+let min = number1 - number2
+let op = number1 * number2
+const deg = number1 / number2
+console.log(`$sum ${sum} ${min} ${op} ${deg}`)*/
+
 //Запросите у пользователя трехзначное число и выведите вторую цифру этого числа. Для решения задачи используйте оператор % (остаток от деления).
 /*let trex = prompt('Введите число', '0');
 let sec = Math.floor((trex / 10) % 10);
@@ -19,6 +50,12 @@ let time_left = (1440 - time_in_min);
 let hour_left = (time_left / 60);
 let minute_left = (time_left % 60);
 alert(hour_left, minute_left);*/
+
+//арплата работника составляет $250 + 10% от продаж. Запросите общую сумму продаж за месяц и посчитайте зарплату.
+/*const salary = 250;
+let sales = +process.argv[2];
+let sum = sales * 0.1;
+console.log(sum + salary);*/
 
 /*let year = process.argv[2];
 switch(year % 4)
