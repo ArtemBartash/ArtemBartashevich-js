6a902aeebf77c7956061d97b529844e0966bb65c
ArtemBartashevich <69053070+ArtemBartash@users.noreply.github.com>
2020-10-17 00:28:12 +0300

    feat: second issue

--- hm-5/main.js
+++ hm-5/main.js
@@ -1,9 +1,26 @@
 //1.Подсчитать сумму всех чисел в заданном пользователем диапазоне.
-let min_number = Number(process.argv[2]);
+/*let min_number = Number(process.argv[2]);
 let max_number = Number(process.argv[3]);
 let sum = 0;
 for(let i = min_number; i <= max_number; i++)
 { 
     sum += i;
 }
-console.log(sum);
+console.log(sum);*/
+
+//2.Запросить 2 числа и найти только наибольший общий делитель.
+/*let first_number = Number(process.argv[2]);
+let second_number = Number(process.argv[3]);
+while(first_number != second_number)
+{
+    if(first_number > second_number)
+    {
+        first_number = first_number - second_number;
+    }
+    else
+    {
+        second_number = second_number - first_number;
+    }
+}
+console.log(first_number);*/
+
