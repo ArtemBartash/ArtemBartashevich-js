e1cc82005e4fbf81882ba4e64e8ee04f2073288a
ArtemBartashevich <69053070+ArtemBartash@users.noreply.github.com>
2020-10-16 23:07:20 +0300

    feat: some code

---
+++ hm-5/index.js
@@ -0,0 +1,32 @@
+/*let n = Number(process.argv[2]);
+let i = 0;
+while(i < n)
+{
+    console.log('#');
+    i++;
+}*/
+
+/*let number = +process.argv[2];
+let pow = +process.argv[3];
+let i = 1;
+while(i <= number)
+{
+    console.log(Math.pow(i, pow));
+    i++;
+}*/
+
+/*let sum = 2 + 2 * 2;
+do
+{
+    let number = +process.argv[2];
+}
+while(sum != number)*/
+
+/*let del = +process.argv[3];
+for(let i = 1; i <= 100; i++)
+{
+    if(i % del === 0)
+    {
+        console.log(i);
+    }
+}*/
